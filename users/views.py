027b1cd9a19a44ee396b78d1ed30455d055aa917
notaCoder69 <52592276+notaCoder69@users.noreply.github.com>
2020-07-12 02:49:54 +0500

    added User login

--- users/views.py
+++ users/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from .forms import UserRegisterForm
 
 def register(request):
@@ -8,9 +9,15 @@ def register(request):
 		if form.is_valid():
 			form.save()
 			username = form.cleaned_data.get('username')
-			messages.success(request, f'Accout created for {username}!')
-			return redirect('blog-home')
+			messages.success(request, f'Account created for {username}. You can now login')
+			return redirect('login')
 	else:
 		form = UserRegisterForm()
 	return render(request, 'users/register.html', {'form': form})
+
+
+
+@login_required
+def profile(request):
+	return render(request, 'users/profile.html')
 
