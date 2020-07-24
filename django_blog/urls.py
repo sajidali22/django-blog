35a29c9cf529a8ce3da2fe68e0db2e7e88c16a29
notaCoder69 <52592276+notaCoder69@users.noreply.github.com>
2020-07-25 02:42:27 +0500

    Added profile page

--- django_blog/urls.py
+++ django_blog/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from users import views as user_views
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
@@ -28,3 +30,7 @@ urlpatterns = [
     path('', include('blog.urls')),
 
 ]
+
+
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
