b5adff3357283c3241eaa377a7b325350cff0d40
notaCoder69 <52592276+notaCoder69@users.noreply.github.com>
2020-08-19 20:13:34 +0500

    added delete and changes to front end

--- blog/views.py
+++ blog/views.py
@@ -3,7 +3,8 @@ from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.views.generic import (ListView,
 		 DetailView,
 		 CreateView,
-		 UpdateView)
+		 UpdateView,
+		 DeleteView)
 from .models import post
 
 # Create your views here.
@@ -40,6 +41,17 @@ class PostUpdateView(LoginRequiredMixin,UserPassesTestMixin, UpdateView):
 	def form_valid(self, form):
 		form.instance.author = self.request.user
 		return super().form_valid(form)
+
+	def test_func(self):
+		post = self.get_object()
+		if self.request.user == post.author:
+			return True
+		return False
+
+
+class PostDeleteView(LoginRequiredMixin,UserPassesTestMixin, DeleteView):
+	model = post
+	success_url = '/'
 
 	def test_func(self):
 		post = self.get_object()
