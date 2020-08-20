b912a662d9f803229851f8973c5f8fc911f482dc
notaCoder69 <52592276+notaCoder69@users.noreply.github.com>
2020-08-20 10:57:00 +0500

    added new sign up page

--- django_blog/urls.py
+++ django_blog/urls.py
@@ -23,8 +23,8 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('register/', user_views.register, name="register"),
-    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name="login"),
-    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name="logout"),
+    #path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name="login"),
+    path('login/', auth_views.LoginView.as_view(template_name='users/login_new.html'), name="login"),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name="logout"),
     path('profile/', user_views.profile, name="profile"),
     path('', include('blog.urls')),
--- users/views.py
+++ users/views.py
@@ -15,6 +15,18 @@ def register(request):
 		form = UserRegisterForm()
 	return render(request, 'users/register.html', {'form': form})
 
+def beta(request):
+	if request.method == 'POST':
+		form = UserRegisterForm(request.POST)
+		if form.is_valid():
+			form.save()
+			username = form.cleaned_data.get('username')
+			messages.success(request, f'Account created for {username}. You can now Sign in')
+	else:
+		form = UserRegisterForm()
+	return render(request, 'users/login_new.html', {'form': form})
+
+
 
 
 @login_required
