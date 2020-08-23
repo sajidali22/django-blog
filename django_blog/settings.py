a03cb51a52d9da75caab4b5bbfde5d4f625fb9d9
notaCoder69 <52592276+notaCoder69@users.noreply.github.com>
2020-08-23 15:52:11 +0500

    updated setings

--- django_blog/settings.py
+++ django_blog/settings.py
@@ -137,7 +137,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'sajidalisad22@gmail.com'
-EMAIL_HOST_PASSWORD = 'pthamfzkeiidewmv'
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 
 AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
@@ -148,3 +148,5 @@ AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+ALLOWED_HOSTS = ['webloggerproject.herokuapp.com']
